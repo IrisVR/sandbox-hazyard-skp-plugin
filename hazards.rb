fa469ee1cbc9206c24bc08e00897439e755d8a7c
Nate Beatty <nate.beatty@gmail.com>
2014-09-13 19:39:22 -0700

    Hazards loading

--- hazards.rb
+++ hazards.rb
@@ -6,18 +6,21 @@ require_relative File.join(libDir, "HazardLib.rb")
 def init
     hazardlib = HazardLib.new()
     hazardlib.create_components
-    hazardlib.add_components_test
+    # hazardlib.add_components_test
 
     toolbar = UI::Toolbar.new "Hazard Constructor"
-    cmd = UI::Command.new("Test") {
-        UI.messagebox "Hello World"
+
+    # create add worker command
+    cworker_cmd = UI::Command.new("Add Worker") {
+        hazardlib.add_component
     }
-    cmd.small_icon = "ToolPencilSmall.png"
-    cmd.large_icon = "ToolPencilLarge.png"
-    cmd.tooltip = "Test Toolbar"
-    cmd.status_bar_text = "Testing the toolbars class"
-    cmd.menu_text = "Test"
-    toolbar = toolbar.add_item cmd
+    cworker_cmd.small_icon = "ToolPencilSmall.png"
+    cworker_cmd.large_icon = "ToolPencilLarge.png"
+    cworker_cmd.tooltip = "Add Worker"
+    cworker_cmd.status_bar_text = "Adding a worker hazard to the model"
+    cworker_cmd.menu_text = "Add Worker"
+    toolbar = toolbar.add_item cworker_cmd
+
     toolbar.show
 
 end #init
--- lib/HazardLib.rb
+++ lib/HazardLib.rb
@@ -15,19 +15,24 @@ class HazardLib
         UI.messagebox coworker_safe.name
     end # create_components
 
-    def add_components_test
+    def add_component(name = "CWorker-Safe")
         entities = Sketchup.active_model.entities
         definitions = Sketchup.active_model.definitions
 
         definitions.each { |definition|
 
-            if definition.name == "CWorker-Safe"
+            if definition.name == name
                 transformation = Geom::Transformation.new([0,0,0])
                 componentinstance = entities.add_instance(definition, transformation)
-                #
-                # status = componentinstance.set_attribute "testdictionary", "test", 115
-                # status = componentinstance.set_attribute "testdictionary", "key", 115
-                #
+
+                # Set the component-specific attribute dictionary
+                case name
+                when "CWorker-Safe"
+                    componentinstance.set_attribute "attributes", "hazardlib", "true"
+                else
+                    componentinstance.set_attribute "attributes", "hazardlib", "true"
+                end
+
                 # value = componentinstance.get_attribute "testdictionary", "test"
                 #
                 # UI.messagebox(value)
